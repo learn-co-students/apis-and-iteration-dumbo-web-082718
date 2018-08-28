819dc2013990a015f6827ae043970f32fb1018c5
McKenzie Fiege <mckenziefiege@McKenzies-MacBook-Pro.local>
2018-08-28 17:18:22 -0400

    finished lab

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,11 +3,26 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-  
-  # NOTE: in this demonstration we name many of the variables _hash or _array. 
+  response_hash["results"].each do |character_info|
+    character_info.each do |key, value|
+      if value == character
+        return character_info["films"]
+      end
+    end
+  end
+end
+
+def title_generater(url)
+    response_string = RestClient.get(url)
+    response_hash = JSON.parse(response_string)
+    response_hash["title"]
+    #binding.pry
+end
+#title_generater("https://www.swapi.co/api/films/5/")
+
+  # NOTE: in this demonstration we name many of the variables _hash or _array.
   # This is done for educational purposes. This is not typically done in code.
 
 
@@ -19,16 +34,20 @@ def get_character_movies_from_api(character)
   #  i.e. an array of hashes in which each hash reps a given film
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-end
+  #  of movies by title. play around with puts out other info about a given film.)
 
-def print_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+def print_movies(character)
+  films_array = get_character_movies_from_api(character)
+  films_array.map.with_index(1) do |film, index|
+    "#{index} " + title_generater(film)
+  end
 end
+#puts print_movies("Luke Skywalker")
+
 
 def show_character_movies(character)
   films_array = get_character_movies_from_api(character)
-  print_movies(films_array)
+  print_movies(character)
 end
 
 ## BONUS
