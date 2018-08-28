9e45dfa941278800bece59d4eec13d3cf4c4f34c
Kat Michaela <k@gothfem.me>
2018-08-28 17:35:33 -0400

    first attempt

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -12,14 +12,33 @@ def get_character_movies_from_api(character)
 end
 
 def get_character_hash(character)
-  get_response["results"].find do |character_hash|
-    character_hash["name"] == character || character_hash["name"].downcase == character
+  all_characters_hash.find do |character_hash|
+        character_hash["name"].downcase == character.downcase
   end
+end
+
+def all_characters_hash
+ big_array = []
+ counter = 1
+ current_page = get_response
+ partial_link = "http://www.swapi.co/api/people/?page="
+ until current_page["next"] == nil
+   current_page = get_response(partial_link+counter.to_s)
+   big_array<<current_page["results"]
+   #big_array << current_page
+   counter += 1
+   #current_page=current_page["next"]
+   #binding.pry
+  end
+  big_array.flatten
 end
 
 def get_response(link = 'http://www.swapi.co/api/people/')
   response_string = RestClient.get(link)
   response_hash = JSON.parse(response_string)
+  # big_array = [response_hash]
+  # until response_hash["next"] == null
+  #   next_string =
   response_hash
 end
 
